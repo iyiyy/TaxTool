f5ab46df6d5015208eae9eb5d17dcb9ad0351342
Roman Birca <romanbirca@gmail.com>
2024-07-22 22:45:17 +0200

    Added DB migrations

---
+++ TaxTool/Migrations/20240719231231_InitDb.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TaxTool.Model.Entity;
+
+#nullable disable
+
+namespace TaxTool.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Municipality",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Municipality", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaxRecord",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FromDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ToDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    TaxRate = table.Column<double>(type: "float", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    MunicipalityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaxRecord", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaxRecord_Municipality_MunicipalityId",
+                        column: x => x.MunicipalityId,
+                        principalTable: "Municipality",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaxRecord_MunicipalityId",
+                table: "TaxRecord",
+                column: "MunicipalityId");
+            
+            var munId = Guid.NewGuid();
+            
+            migrationBuilder.InsertData(
+                table: "Municipality",
+                columns: new[] {  "Id", "Name" },
+                values: new object[] { munId, "Copenhagen" }
+            );
+ 
+            migrationBuilder.InsertData(
+                table: "TaxRecord",
+                columns: new[]
+                {
+                    "Id",
+                    "MunicipalityId",
+                    "FromDate",
+                    "ToDate",
+                    "TaxRate",
+                    "Type"
+                },
+                values: new object[] { 
+                    Guid.NewGuid(),
+                    munId,
+                    DateTimeOffset.ParseExact("2024.01.01 00:00:00 UTC",  "yyyy.MM.dd HH:mm:ss 'UTC'", new CultureInfo("en-US")),
+                    DateTimeOffset.ParseExact("2024.12.31 23:59:59 UTC",  "yyyy.MM.dd HH:mm:ss 'UTC'", new CultureInfo("en-US")),
+                    0.2,
+                    2
+                }
+            );
+            
+            migrationBuilder.InsertData(
+                table: "TaxRecord",
+                columns: new[]
+                {
+                    "Id",
+                    "MunicipalityId",
+                    "FromDate",
+                    "ToDate",
+                    "TaxRate",
+                    "Type"
+                },
+                values: new object[] { 
+                    Guid.NewGuid(),
+                    munId,
+                    DateTimeOffset.ParseExact("2024.05.01 00:00:00 UTC",  "yyyy.MM.dd HH:mm:ss 'UTC'", new CultureInfo("da-DK")),
+                    DateTimeOffset.ParseExact("2024.05.31 23:59:59 UTC",  "yyyy.MM.dd HH:mm:ss 'UTC'", new CultureInfo("da-DK")),
+                    0.4,
+                    0
+                }
+            );
+            
+            migrationBuilder.InsertData(
+                table: "TaxRecord",
+                columns: new[]
+                {
+                    "Id",
+                    "MunicipalityId",
+                    "FromDate",
+                    "ToDate",
+                    "TaxRate",
+                    "Type"
+                },
+                values: new object[] { 
+                    Guid.NewGuid(),
+                    munId,
+                    DateTimeOffset.ParseExact("2024.01.01 00:00:00 UTC",  "yyyy.MM.dd HH:mm:ss 'UTC'", new CultureInfo("en-US")),
+                    DateTimeOffset.ParseExact("2024.01.01 23:59:59 UTC",  "yyyy.MM.dd HH:mm:ss 'UTC'", new CultureInfo("en-US")),
+                    0.1,
+                    1
+                }
+            );
+            
+            migrationBuilder.InsertData(
+                table: "TaxRecord",
+                columns: new[]
+                {
+                    "Id",
+                    "MunicipalityId",
+                    "FromDate",
+                    "ToDate",
+                    "TaxRate",
+                    "Type"
+                },
+                values: new object[] { 
+                    Guid.NewGuid(),
+                    munId,
+                    DateTimeOffset.ParseExact("2024.12.25 00:00:00 UTC",  "yyyy.MM.dd HH:mm:ss 'UTC'", new CultureInfo("en-US")),
+                    DateTimeOffset.ParseExact("2024.12.25 23:59:59 UTC",  "yyyy.MM.dd HH:mm:ss 'UTC'", new CultureInfo("en-US")),
+                    0.1,
+                    1
+                }
+            );
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaxRecord");
+
+            migrationBuilder.DropTable(
+                name: "Municipality");
+        }
+    }
+}
---
+++ TaxTool/outputscript.sql
@@ -0,0 +1,82 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Municipality] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Municipality] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [TaxRecord] (
+    [Id] uniqueidentifier NOT NULL,
+    [FromDate] datetimeoffset NOT NULL,
+    [ToDate] datetimeoffset NOT NULL,
+    [TaxRate] float NOT NULL,
+    [Type] int NOT NULL,
+    [MunicipalityId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_TaxRecord] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_TaxRecord_Municipality_MunicipalityId] FOREIGN KEY ([MunicipalityId]) REFERENCES [Municipality] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_TaxRecord_MunicipalityId] ON [TaxRecord] ([MunicipalityId]);
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Name') AND [object_id] = OBJECT_ID(N'[Municipality]'))
+    SET IDENTITY_INSERT [Municipality] ON;
+INSERT INTO [Municipality] ([Id], [Name])
+VALUES ('1bc65c4f-d901-4f7e-9b5a-478191ae7d4a', N'Copenhagen');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Name') AND [object_id] = OBJECT_ID(N'[Municipality]'))
+    SET IDENTITY_INSERT [Municipality] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'MunicipalityId', N'FromDate', N'ToDate', N'TaxRate', N'Type') AND [object_id] = OBJECT_ID(N'[TaxRecord]'))
+    SET IDENTITY_INSERT [TaxRecord] ON;
+INSERT INTO [TaxRecord] ([Id], [MunicipalityId], [FromDate], [ToDate], [TaxRate], [Type])
+VALUES ('a189e49a-35f6-4423-80c2-7cc7b1d73486', '1bc65c4f-d901-4f7e-9b5a-478191ae7d4a', '2024-01-01T00:00:00.0000000+01:00', '2024-12-31T23:59:59.0000000+01:00', 0.20000000000000001E0, 2);
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'MunicipalityId', N'FromDate', N'ToDate', N'TaxRate', N'Type') AND [object_id] = OBJECT_ID(N'[TaxRecord]'))
+    SET IDENTITY_INSERT [TaxRecord] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'MunicipalityId', N'FromDate', N'ToDate', N'TaxRate', N'Type') AND [object_id] = OBJECT_ID(N'[TaxRecord]'))
+    SET IDENTITY_INSERT [TaxRecord] ON;
+INSERT INTO [TaxRecord] ([Id], [MunicipalityId], [FromDate], [ToDate], [TaxRate], [Type])
+VALUES ('f52ee1c4-8907-4c17-b31d-0004b38b3853', '1bc65c4f-d901-4f7e-9b5a-478191ae7d4a', '2024-05-01T00:00:00.0000000+02:00', '2024-05-31T23:59:59.0000000+02:00', 0.40000000000000002E0, 0);
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'MunicipalityId', N'FromDate', N'ToDate', N'TaxRate', N'Type') AND [object_id] = OBJECT_ID(N'[TaxRecord]'))
+    SET IDENTITY_INSERT [TaxRecord] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'MunicipalityId', N'FromDate', N'ToDate', N'TaxRate', N'Type') AND [object_id] = OBJECT_ID(N'[TaxRecord]'))
+    SET IDENTITY_INSERT [TaxRecord] ON;
+INSERT INTO [TaxRecord] ([Id], [MunicipalityId], [FromDate], [ToDate], [TaxRate], [Type])
+VALUES ('0dca0120-dfe1-43a6-b7e3-bfd6fe1aa79c', '1bc65c4f-d901-4f7e-9b5a-478191ae7d4a', '2024-01-01T00:00:00.0000000+01:00', '2024-01-01T23:59:59.0000000+01:00', 0.10000000000000001E0, 1);
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'MunicipalityId', N'FromDate', N'ToDate', N'TaxRate', N'Type') AND [object_id] = OBJECT_ID(N'[TaxRecord]'))
+    SET IDENTITY_INSERT [TaxRecord] OFF;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'MunicipalityId', N'FromDate', N'ToDate', N'TaxRate', N'Type') AND [object_id] = OBJECT_ID(N'[TaxRecord]'))
+    SET IDENTITY_INSERT [TaxRecord] ON;
+INSERT INTO [TaxRecord] ([Id], [MunicipalityId], [FromDate], [ToDate], [TaxRate], [Type])
+VALUES ('4023b0c5-9a91-4ded-9fab-3b071270b096', '1bc65c4f-d901-4f7e-9b5a-478191ae7d4a', '2024-12-25T00:00:00.0000000+01:00', '2024-12-25T23:59:59.0000000+01:00', 0.10000000000000001E0, 1);
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'MunicipalityId', N'FromDate', N'ToDate', N'TaxRate', N'Type') AND [object_id] = OBJECT_ID(N'[TaxRecord]'))
+    SET IDENTITY_INSERT [TaxRecord] OFF;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240719231231_InitDb', N'8.0.7');
+GO
+
+COMMIT;
+GO
+
